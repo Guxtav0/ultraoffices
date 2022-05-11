68b8e198fec428dfd57c526a40f30cfdec5a8893
Gustavo Mariano <guxtavo.contato@gmail.com>
2022-05-11 20:27:18 -0300

    segundo commit

--- navbar.js
+++ navbar.js
@@ -6,20 +6,17 @@ class navBar{
         this.active ="active";
         this.handleClick = this.handleClick.bind(this);
     }
-    animateLinks(){
-        this.navLinks.forEach((link) =>{
-            link.style.animation ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease fowards 0.3s`);
-        });
-    }
     handleClick(){
         this.navList.classList.toggle(this.active);
-        this.animateLinks();
     }
     addClick(){
         this.mobilemenu.addEventListener("click", this.handleClick);
+        this.navLinks.addEventListener("click", this.handleClick);
     }
     init(){
         if(this.mobilemenu){
+            this.addClick();
+        }else if(this.navLinks){
             this.addClick();
         }
         return this;
